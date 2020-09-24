e9343ff54a578d652071341e44d02ea0608e40f2
alban_favre <fav.alban2@gmail.com>
2020-09-24 11:19:27 +0200

    basic methods webzone user

--- src/main/java/ch/heigvd/amt/project/application/RegisterCommand.java
+++ src/main/java/ch/heigvd/amt/project/application/RegisterCommand.java
@@ -1,4 +1,11 @@
 package ch.heigvd.amt.project.application;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+@Builder // lié a lambok
+@Getter
 public class RegisterCommand {
+    private String username;
+    private String password;
+    private String confirmePassword;
 }
