bc9713f162137145b169a6f20fe0caf5aecbf393
Zaretti <g.zaretti@gmail.com>
2020-09-28 21:23:57 +0200

    deployment pipeline openliberty

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp ../target/stackoverflow-simplified.war images/payara/apps
 cp ../target/stackoverflow-simplified.war images/wildfly/apps
+cp ../target/stackoverflow-simplified.war images/openliberty/apps
 cd topologies
 docker-compose up --build
