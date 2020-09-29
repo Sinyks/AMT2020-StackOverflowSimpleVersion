e9cbbded1458d1576e83fbe2e64def1c82ac6de8
Zaretti <g.zaretti@gmail.com>
2020-09-29 21:00:34 +0200

    update pipline

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cp ../target/stackoverflow-simplified.war images/payara/apps
-cp ../target/stackoverflow-simplified.war images/wildfly/apps
 cp ../target/stackoverflow-simplified.war images/openliberty/apps
+cp ../src/main/liberty/config/server.xml images/openliberty/configs
 cd topologies
 docker-compose up --build
