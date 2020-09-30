89436f1c2be856466391466e943e73fa727eb311
sacha.perdrizat <sacha.perdrizat@gmail.com>
2020-10-01 00:31:37 +0200

    correct and enhanced test e2e

--- e2e/Homepage_test.js
+++ e2e/Homepage_test.js
@@ -1,7 +1,18 @@
+const { I, Wordlist } = inject();
+
 Feature('Homepage');
 
-Scenario('Visit the Homepage', (I) => {
-  I.amOnPage('/');
+Scenario('Visit the Homepage', (I, Wordlist) => {
+  I.amOnPage(Wordlist.pageUrl.root);
   I.see('Welcome');
   // pause();
+});
+
+Scenario('Click on \"Browse Questions\" Button must redirect me to Questions',(I, Wordlist) => {
+  I.amOnPage(Wordlist.pageUrl.root);
+  I.click("Browse questions");
+  /**
+   * controle questions page
+   */
+
 });
