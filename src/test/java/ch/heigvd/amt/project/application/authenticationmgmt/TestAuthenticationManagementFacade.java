0c82ddb8059195ad6d46451e60806fc9a0b314cb
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2020-10-06 15:16:33 +0200

    WIP Unit Testing

--- src/test/java/ch/heigvd/amt/project/application/authenticationmgmt/TestAuthenticationManagementFacade.java
+++ src/test/java/ch/heigvd/amt/project/application/authenticationmgmt/TestAuthenticationManagementFacade.java
@@ -1,22 +1,30 @@
 package ch.heigvd.amt.project.application.authenticationmgmt;
 
+import ch.heigvd.amt.project.application.authenticationmgmt.login.LoginCommand;
+import ch.heigvd.amt.project.application.authenticationmgmt.login.LoginFailedException;
 import ch.heigvd.amt.project.application.authenticationmgmt.register.RegisterCommand;
 import ch.heigvd.amt.project.application.authenticationmgmt.register.RegisterFailedException;
 import ch.heigvd.amt.project.infrastructure.persistence.InMemoryPersonRepository;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAuthenticationManagementFacade {
 
+    private final String username = "User";
+    private final String password = "pass";
+
+    private final String passwordMatchError = "Passwords don't match";
+    private final  String nonExistingUserError = "User not found";
+    private final  String incorrectPasswordError = "Credentials verification failed";
+
+    private final AuthenticationManagementFacade amf = new AuthenticationManagementFacade(new InMemoryPersonRepository());
+
     @Test
     public void registerUserMustReturnNonNullObject() {
-        AuthenticationManagementFacade amf = new AuthenticationManagementFacade(new InMemoryPersonRepository());
 
         CurrentUserDTO cUser = null;
         try {
-            String username = "Didier";
-            String password = "pass";
             cUser = amf.register(RegisterCommand.builder()
                                                 .username(username)
                                                 .clearTextPassword(password)
@@ -28,5 +36,58 @@ public class TestAuthenticationManagementFacade {
         }
 
         assertNotNull(cUser);
+    }
+
+    @Test
+    public void registerUserMustThrowErrorIfUnmatchPassword(){
+
+        CurrentUserDTO cUser = null;
+        try {
+            amf.register(RegisterCommand.builder()
+                                        .username(username)
+                                        .clearTextPassword(password)
+                                        .clearTextPasswordConfirm("notmatch")
+                                        .build()
+            );
+        } catch (RegisterFailedException e) {
+            e.printStackTrace();
+            assertEquals(passwordMatchError,e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void loginWithExistigUserMustReturnNonNullObject(){
+        CurrentUserDTO cUser = null;
+
+        try {
+            cUser = amf.login(LoginCommand.builder().username(username).clearTextPassword(password).build());
+        } catch (LoginFailedException e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        assertNotNull(cUser);
+    }
+
+    @Test
+    public void loginWithIncorrectPasswordMustThrowError(){
+        try {
+            amf.login(LoginCommand.builder().username(username).clearTextPassword("error").build());
+        } catch (LoginFailedException e) {
+            e.printStackTrace();
+            assertEquals(incorrectPasswordError,e.getMessage());
+        }
+    }
+
+    @Test
+    public void loginWithNonExistingUserMustThrowError(){
+        try {
+            amf.login(LoginCommand.builder().username("null").clearTextPassword("null").build());
+        } catch (LoginFailedException e) {
+            e.printStackTrace();
+            assertEquals(nonExistingUserError,e.getMessage());
+        }
+
     }
 }
--- src/test/java/ch/heigvd/amt/project/application/questionmgmt/TestQuestionManagementFacade.java
+++ src/test/java/ch/heigvd/amt/project/application/questionmgmt/TestQuestionManagementFacade.java
@@ -1,4 +1,24 @@
 package ch.heigvd.amt.project.application.questionmgmt;
 
+import ch.heigvd.amt.project.application.questionmgmt.ask.AskCommand;
+import ch.heigvd.amt.project.application.questionmgmt.ask.AskFailedException;
+import ch.heigvd.amt.project.infrastructure.persistence.InMemoryQuestionRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class TestQuestionManagementFacade {
+
+    private final QuestionManagementFacade qmf = new QuestionManagementFacade(new InMemoryQuestionRepository());
+
+    @Test
+    public void askQuestionMustThrowErrorIf(){
+        try {
+            qmf.ask(AskCommand.builder().build());
+        } catch (AskFailedException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
 }
