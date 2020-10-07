734f29a744045d4d3b53bc9ee0f30db0d7e42a52
sacha.perdrizat <sacha.perdrizat@gmail.com>
2020-10-07 23:25:41 +0200

    add BeforEach to setup Store

--- src/test/java/ch/heigvd/amt/project/application/authenticationmgmt/TestAuthenticationManagementFacade.java
+++ src/test/java/ch/heigvd/amt/project/application/authenticationmgmt/TestAuthenticationManagementFacade.java
@@ -4,31 +4,44 @@ import ch.heigvd.amt.project.application.authenticationmgmt.login.LoginCommand;
 import ch.heigvd.amt.project.application.authenticationmgmt.login.LoginFailedException;
 import ch.heigvd.amt.project.application.authenticationmgmt.register.RegisterCommand;
 import ch.heigvd.amt.project.application.authenticationmgmt.register.RegisterFailedException;
+import ch.heigvd.amt.project.domain.person.Person;
 import ch.heigvd.amt.project.infrastructure.persistence.InMemoryPersonRepository;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAuthenticationManagementFacade {
 
-    private final String username = "User";
-    private final String password = "pass";
+    private final String newUsername = "Vincent";
+    private final String newPassword = "pass";
+
+    private final String loginUsername = "User";
+    private final String loginPassword = "pass";
 
     private final String passwordMatchError = "Passwords don't match";
     private final  String nonExistingUserError = "User not found";
     private final  String incorrectPasswordError = "Credentials verification failed";
 
-    private final AuthenticationManagementFacade amf = new AuthenticationManagementFacade(new InMemoryPersonRepository());
+    private AuthenticationManagementFacade amf = null;
+
+    @BeforeEach
+    public void loadStore(){
+        Person testPerson = Person.builder().username(loginUsername).clearTextPassword(loginPassword).build();
+        InMemoryPersonRepository testStore = new InMemoryPersonRepository();
+        testStore.save(testPerson);
+        amf = new AuthenticationManagementFacade(testStore);
+    }
 
     @Test
-    public void registerUserMustReturnNonNullObject() {
+    public void registerUserMustReturnValidObject() {
 
         CurrentUserDTO cUser = null;
         try {
             cUser = amf.register(RegisterCommand.builder()
-                                                .username(username)
-                                                .clearTextPassword(password)
-                                                .clearTextPasswordConfirm(password)
+                                                .username(newUsername)
+                                                .clearTextPassword(newPassword)
+                                                .clearTextPasswordConfirm(newPassword)
                                                 .build());
         } catch (RegisterFailedException e) {
             e.printStackTrace();
@@ -36,6 +49,7 @@ public class TestAuthenticationManagementFacade {
         }
 
         assertNotNull(cUser);
+        assertEquals(cUser.getUsername(),newUsername);
     }
 
     @Test
@@ -44,8 +58,8 @@ public class TestAuthenticationManagementFacade {
         CurrentUserDTO cUser = null;
         try {
             amf.register(RegisterCommand.builder()
-                                        .username(username)
-                                        .clearTextPassword(password)
+                                        .username(newUsername)
+                                        .clearTextPassword(newPassword)
                                         .clearTextPasswordConfirm("notmatch")
                                         .build()
             );
@@ -61,7 +75,7 @@ public class TestAuthenticationManagementFacade {
         CurrentUserDTO cUser = null;
 
         try {
-            cUser = amf.login(LoginCommand.builder().username(username).clearTextPassword(password).build());
+            cUser = amf.login(LoginCommand.builder().username(loginUsername).clearTextPassword(loginPassword).build());
         } catch (LoginFailedException e) {
             e.printStackTrace();
             fail();
@@ -73,7 +87,7 @@ public class TestAuthenticationManagementFacade {
     @Test
     public void loginWithIncorrectPasswordMustThrowError(){
         try {
-            amf.login(LoginCommand.builder().username(username).clearTextPassword("error").build());
+            amf.login(LoginCommand.builder().username(loginUsername).clearTextPassword("error").build());
         } catch (LoginFailedException e) {
             e.printStackTrace();
             assertEquals(incorrectPasswordError,e.getMessage());
