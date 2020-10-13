6750c1565bc57129e58f607f01e657da510ee70f
MaximilianCVogel <maximilian.c.vogel@gmail.com>
2020-10-13 12:07:10 +0200

    changed and commented the ServiceRegistry to be more up to date, not tested

--- src/main/java/ch/heigvd/amt/project/application/ServiceRegistry.java
+++ src/main/java/ch/heigvd/amt/project/application/ServiceRegistry.java
@@ -7,25 +7,50 @@ import ch.heigvd.amt.project.domain.question.IQuestionRepository;
 import ch.heigvd.amt.project.infrastructure.persistence.inMemory.InMemoryUserRepository;
 import ch.heigvd.amt.project.infrastructure.persistence.inMemory.InMemoryQuestionRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@ApplicationScoped
 public class ServiceRegistry {
 
+    /** Putting this service registry stuff aside for now, we shouldn't need it anymore
     private static final ServiceRegistry SERVICE_REGISTRY = new ServiceRegistry();
 
+    public static ServiceRegistry getServiceRegistry(){
+        return SERVICE_REGISTRY;
+    }
+     */
+
+    //Injecting the repositories, maybe we need to do it for the facades too, but not everything at once
+    @Inject @Named("PgsqlUserRepository")
+    IUserRepository userRepository;
+
+    @Inject @Named("InMemoryQuestionRepository")
+    IQuestionRepository questionRepository;
+
+    /** Creating these facades as before doesn't make sense with our get functions, so we'll put these aside for now :
+     *  we're basically creating a facade instance whenever someone makes a call to get it (probably not optimal)
+    private static QuestionsManagementFacade questionsManagementFacade;
+    private static AuthenticationManagementFacade authenticationManagementFacade;
+     */
+
+    public AuthenticationManagementFacade getAuthenticationManagementFacade(){
+        return new AuthenticationManagementFacade(userRepository);
+    }
+
+    public QuestionsManagementFacade getQuestionManagementFacade(){
+        return new QuestionsManagementFacade(questionRepository);
+    }
+
+    //this is the old implementation for the service listing
+
+    /**
     private static final IUserRepository I_USER_REPOSITORY = new InMemoryUserRepository();
     private static final IQuestionRepository I_QUESTION_REPOSITORY = new InMemoryQuestionRepository();
 
     private static final AuthenticationManagementFacade AUTHENTICATION_MANAGEMENT_FACADE = new AuthenticationManagementFacade(I_USER_REPOSITORY);
     private static final QuestionsManagementFacade QUESTION_MANAGEMENT_FACADE = new QuestionsManagementFacade(I_QUESTION_REPOSITORY);
+    */
 
-    public static ServiceRegistry getServiceRegistry(){
-        return SERVICE_REGISTRY;
-    }
-
-    public static AuthenticationManagementFacade getAuthenticationManagementFacade(){
-        return AUTHENTICATION_MANAGEMENT_FACADE;
-    }
-
-    public static QuestionsManagementFacade getQuestionManagementFacade(){
-        return QUESTION_MANAGEMENT_FACADE;
-    }
 }
