8057788f60172289ab5310d971b1370b67b01bf2
MaximilianCVogel <maximilian.c.vogel@gmail.com>
2020-10-21 23:10:49 +0200

    added comment domain

---
+++ src/main/java/ch/heigvd/amt/project/domain/comment/Comment.java
@@ -0,0 +1,62 @@
+package ch.heigvd.amt.project.domain.comment;
+
+import ch.heigvd.amt.project.domain.IEntity;
+import ch.heigvd.amt.project.domain.answer.AnswerId;
+import ch.heigvd.amt.project.domain.question.QuestionId;
+import ch.heigvd.amt.project.domain.user.UserId;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Builder(toBuilder = true)
+public class Comment implements IEntity<Comment, CommentId> {
+    @Setter(AccessLevel.NONE)
+    private CommentId id;
+
+    private UserId userId;
+    private AnswerId answerId;
+    private QuestionId questionId;
+    private Date creationDate;
+    private Date lastEditDate;
+    private String body;
+
+    @Override
+    public Comment deepClone() {
+        return this.toBuilder()
+                .id(new CommentId(id.asString()))
+                .build();
+    }
+
+    public static class CommentBuilder{
+        public Comment build(){
+            if(id == null){
+                id = new CommentId();
+            }
+            if(userId==null){
+                throw new IllegalArgumentException("userId mandatory");
+            }
+            if(answerId == null && questionId == null){
+                throw new IllegalArgumentException("answerId or questionId mandatory");
+            }
+            if(answerId != null && questionId != null){
+                throw new IllegalArgumentException("one and only one of answerId or questionId");
+            }
+            if(creationDate == null){
+                creationDate=Date.from(Instant.now());
+            }
+            if(lastEditDate == null){
+                lastEditDate=creationDate;
+            }
+            if(body == null || body.isEmpty()){
+                throw new IllegalArgumentException("body mandatory");
+            }
+
+            Comment newComment = new Comment(id, userId, answerId, questionId, creationDate, lastEditDate, body);
+            return newComment;
+        }
+    }
+}
