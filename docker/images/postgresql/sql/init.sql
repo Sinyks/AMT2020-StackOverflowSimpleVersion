58a94d1c5c0235fda5c2ea38d1af68a598959742
Zaretti <g.zaretti@gmail.com>
2020-10-22 10:46:47 +0200

    sql for questions, answers, comments   -> ok

--- docker/images/postgresql/sql/init.sql
+++ docker/images/postgresql/sql/init.sql
@@ -7,7 +7,46 @@ CREATE TABLE users(
     email character varying(255) NOT NULL,
     aboutMe TEXT,
     password character varying(255) NOT NULL,
-    CONSTRAINT ct_pk_user PRIMARY KEY(pk_user),
-    CONSTRAINT ct_uk_username UNIQUE(username),
-    CONSTRAINT ct_uk_email UNIQUE(email)
+    CONSTRAINT usersct_pk_user PRIMARY KEY(pk_user),
+    CONSTRAINT usersct_uk_username UNIQUE(username),
+    CONSTRAINT usersct_uk_email UNIQUE(email)
 );
+
+CREATE TABLE questions(
+    pk_question uuid NOT NULL,
+    fk_ownerId uuid NOT NULL,
+    title  character varying(255) NOT NULL,
+    creationDate timestamp NOT NULL,
+    lastEditDate timestamp NOT NULL,
+    body TEXT NOT NULL,
+    CONSTRAINT questionsct_pk_question PRIMARY KEY(pk_question),
+    CONSTRAINT questionsct_pk_fk_ownerId FOREIGN KEY(fk_ownerId) REFERENCES users(pk_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT questionsct_uk_title UNIQUE(title)
+);
+
+CREATE TABLE answers(
+    pk_answer uuid NOT NULL,
+    fk_ownerId uuid NOT NULL,
+    fk_questionId uuid NOT NULL,
+    creationDate timestamp NOT NULL,
+    lastEditDate timestamp NOT NULL,
+    body TEXT NOT NULL,
+    CONSTRAINT answersct_pk_question PRIMARY KEY(pk_answer),
+    CONSTRAINT answersct_pk_fk_ownerId FOREIGN KEY(fk_ownerId) REFERENCES users(pk_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT answersct_pk_fk_questionId FOREIGN KEY(fk_questionId) REFERENCES questions(pk_question)  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE comments(
+    pk_comment uuid NOT NULL,
+    fk_ownerId uuid NOT NULL,
+    fk_questionId uuid NULL,
+    fk_answerId uuid NULL,
+    creationDate timestamp NOT NULL,
+    lastEditDate timestamp NOT NULL,
+    body TEXT NOT NULL,
+    CONSTRAINT comments_pk_question PRIMARY KEY(pk_comment),
+    CONSTRAINT comments_pk_fk_ownerId FOREIGN KEY(fk_ownerId) REFERENCES users(pk_user) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT comments_pk_fk_questionId FOREIGN KEY(fk_questionId) REFERENCES questions(pk_question)  ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT comments_pk_fk_answerId FOREIGN KEY(fk_answerId) REFERENCES answers(pk_answer)  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
