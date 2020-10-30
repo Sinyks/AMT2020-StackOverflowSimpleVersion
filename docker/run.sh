121a3d425c2c809375d85fff5e17e5f8d0ae3410
Sacha <sacha.perdrizat@gmail.com>
2020-10-30 10:51:41 +0100

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp ../target/stackoverflow-simplified.war images/openliberty/apps
 cp ../src/main/liberty/config/server.xml images/openliberty/configs
+cp ../src/main/liberty/config/postgresql*.jar images/openliberty/configs
 cd topologies
 docker-compose up
