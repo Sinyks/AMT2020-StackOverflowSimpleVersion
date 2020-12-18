24df2efafea94616cce0dc5cccec11ab2a67e4f7
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2020-12-18 11:43:35 +0100

    add setup script files

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# docker-compose up topologie API rest
+
+# script setup python/js
+# create Application and get application key
+# Set badges/Pointscale/rules
+# save API-key to .env
+
+# launch stackoverflow toplogies with api key as env variable
