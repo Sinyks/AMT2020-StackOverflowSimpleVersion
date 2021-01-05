3095cb754ce42621689c51837ac4ec66a085924f
MaximilianCVogel <maximilian.c.vogel@gmail.com>
2021-01-05 19:27:29 +0100

    added GET requests to get the JSON reputation objects

--- src/main/java/ch/heigvd/amt/project/ui/web/HomePageServlet.java
+++ src/main/java/ch/heigvd/amt/project/ui/web/HomePageServlet.java
@@ -1,6 +1,10 @@
 package ch.heigvd.amt.project.ui.web;
 
 import ch.heigvd.amt.project.application.ServiceRegistry;
+import ch.heigvd.amt.project.application.authenticationmgmt.CurrentUserDTO;
+import ch.heigvd.amt.project.dotenv.DotenvManager;
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.SneakyThrows;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -9,6 +13,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 @WebServlet(urlPatterns = "", name = "HomePage")
 public class HomePageServlet extends HttpServlet {
@@ -16,6 +25,7 @@ public class HomePageServlet extends HttpServlet {
     @Inject
     ServiceRegistry serviceRegistry;
 
+    @SneakyThrows
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -23,6 +33,30 @@ public class HomePageServlet extends HttpServlet {
         int userCount = (serviceRegistry.getAuthenticationManagementFacade()).getUserCount();
         int questionCount = (serviceRegistry.getQuestionManagementFacade()).getQuestionCount();
         int answerCount = (serviceRegistry.getAnswerManagementFacade()).getAnswerCount();
+
+        //Get ENV variables for setting the requests
+        Dotenv dotenv = DotenvManager.getDotenv();
+
+        String api_key = dotenv.get("API_KEY");
+        String api_host = dotenv.get("API_HOST");
+        String api_port = dotenv.get("API_PORT");
+
+        String address = "http://" + api_host + ":" + api_port + "/users/reputations";
+
+        //GET user reputation request
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(address))
+                .setHeader("X-API-KEY", api_key)
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpResponse<String> apiResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
         request.setAttribute("userCount", userCount);
         request.setAttribute("questionCount", questionCount);
--- src/main/java/ch/heigvd/amt/project/ui/web/profile/ProfilePageServlet.java
+++ src/main/java/ch/heigvd/amt/project/ui/web/profile/ProfilePageServlet.java
@@ -1,19 +1,55 @@
 package ch.heigvd.amt.project.ui.web.profile;
 
+import ch.heigvd.amt.project.application.authenticationmgmt.CurrentUserDTO;
+import ch.heigvd.amt.project.dotenv.DotenvManager;
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.SneakyThrows;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 @WebServlet(urlPatterns = "/profile", name = "ProfilePage")
 public class ProfilePageServlet extends HttpServlet {
 
 
+    @SneakyThrows
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
+        //Get ENV variables for setting the requests
+        Dotenv dotenv = DotenvManager.getDotenv();
+
+        String api_key = dotenv.get("API_KEY");
+        String api_host = dotenv.get("API_HOST");
+        String api_port = dotenv.get("API_PORT");
+
+        String username = ((CurrentUserDTO) req.getSession().getAttribute("currentUser")).getUsername();
+        String address = "http://" + api_host + ":" + api_port + "/users/" + username + "/reputation";
+
+        //GET user reputation request
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(address))
+                .setHeader("X-API-KEY", api_key)
+                .header("Content-Type", "application/json")
+                .build();
+
+        HttpResponse<String> apiResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
         req.getRequestDispatcher("/WEB-INF/views/Profile.jsp").forward(req, resp);
     }
