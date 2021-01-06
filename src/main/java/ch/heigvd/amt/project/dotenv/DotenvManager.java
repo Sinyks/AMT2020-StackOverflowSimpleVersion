f654ec29b619f8c2a7a77d1619775bc20a4326d3
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2021-01-06 11:20:03 +0100

    comment dotenv config

--- src/main/java/ch/heigvd/amt/project/dotenv/DotenvManager.java
+++ src/main/java/ch/heigvd/amt/project/dotenv/DotenvManager.java
@@ -3,12 +3,12 @@ package ch.heigvd.amt.project.dotenv;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public abstract class DotenvManager {
-    private static String ENV_PATH = "./";
+    //private static String ENV_PATH = "./";
 
     public static Dotenv getDotenv(){
 
         return Dotenv.configure()
-                .directory(ENV_PATH)
+                //.directory(ENV_PATH)
                 .ignoreIfMalformed()
                 .ignoreIfMissing()
                 .filename(".env")
